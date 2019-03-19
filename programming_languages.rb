9c3422a2a22f9c6b2f6b17dfd045fb46f318c1d5
Kate F <follett.katie@gmail.com>
2019-03-19 20:25:18 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -11,11 +11,17 @@ require 'pry'
   # :scala=>{:type=>"compiled"},
   # :javascript=>{:type=>"interpreted"}}}
 def reformat_languages(languages)
-  new_hash = {}
-  languages.each do |programming, language_data|
-    language_data.each do |the_language, type|
-      the_language
-  
+  new_hash = {} 
+   languages.each do |style, description|
+    description.each do |lang, type|
+      if new_hash.has_key?(lang)
+        new_hash[lang][:style] << style
+      else
+        new_hash[lang] = type
+        new_hash[lang][:style] = [style]
+      end
     end
   end
-end
+
+   new_hash  
+end	
